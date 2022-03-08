19a34153a6b10bd63194f4c901e5268505399ab1
verci <vercik.buzkova@centrum.cz>
2022-03-08 17:14:38 +0100

    Přidejte soubory projektu.

---
+++ Ppt_01/VybaveniModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Ppt_01
+{
+    public class VybaveniModel
+    {
+        public string name { get; set; }
+        public DateTime BoughtData { get; set; }
+        public DateTime LastRevisionDate { get; set; }
+        public bool NeedsRevision { get; set; }
+
+        public VybaveniModel(string name, DateTime BoughtData, DateTime LastRevisionDate, bool NeedsRevision)
+        {
+            this.name = name;
+            this.BoughtData = BoughtData;
+            this.LastRevisionDate = LastRevisionDate;
+            this.NeedsRevision = NeedsRevision;
+        }
+    }
+}
