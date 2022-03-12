f9e39dfb4f5c829d2810b16fdfce25371426bcaa
verci <vercik.buzkova@centrum.cz>
2022-03-12 11:47:46 +0100

    Domaci ukol 4 verze 1

--- PptNemocnice.Shared/VybaveniModel.cs
+++ PptNemocnice.Shared/VybaveniModel.cs
@@ -1,18 +1,20 @@
-﻿namespace Ppt_01
+﻿namespace PptNemocnice.Shared
 {
     public class VybaveniModel
     {
         public string name { get; set; }
         public DateTime BoughtData { get; set; }
         public DateTime LastRevisionDate { get; set; }
-        public bool NeedsRevision { get; set; }
+        public bool NeedsRevision => DateTime.Now - LastRevisionDate > TimeSpan.FromDays(365 * 2);
 
-        public VybaveniModel(string name, DateTime BoughtData, DateTime LastRevisionDate, bool NeedsRevision)
+        public bool IsInEditMode { get; set; }
+
+        public VybaveniModel(string name, DateTime BoughtData, DateTime LastRevisionDate)
         {
             this.name = name;
             this.BoughtData = BoughtData;
             this.LastRevisionDate = LastRevisionDate;
-            this.NeedsRevision = NeedsRevision;
+            IsInEditMode = false;
         }
     }
 }
