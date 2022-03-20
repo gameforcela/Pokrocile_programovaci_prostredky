7d50e20c427e3d8e06896158f3f1fe3729b0b7c4
verci <vercik.buzkova@centrum.cz>
2022-03-20 21:02:50 +0100

    5 cviceni

--- PptNemocnice.Shared/VybaveniModel.cs
+++ PptNemocnice.Shared/VybaveniModel.cs
@@ -1,20 +1,67 @@
-﻿namespace PptNemocnice.Shared
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PptNemocnice.Shared;
+public class VybaveniModel
 {
-    public class VybaveniModel
+
+
+
+    [Required, MinLength(5, ErrorMessage = "Délka u pole \"{0}\" musí být alespoň {1} znaků")]
+    [Display(Name = "Název")]
+    public string Name { get; set; } = "";
+
+    [Range(0, 10000000, ErrorMessage = "Cena má být 0 až 10 000 000.")]
+    public double PriceCzk { get; set; }
+    public DateTime BoughtDateTime { get; set; }
+    public DateTime LastRevision { get; set; }
+
+    public bool NeedsRevision => DateTime.Now - LastRevision > TimeSpan.FromDays(365 * 2);
+
+    public bool IsInEditMode { get; set; }
+    public static List<VybaveniModel> GetTestList()
     {
-        public string name { get; set; }
-        public DateTime BoughtData { get; set; }
-        public DateTime LastRevisionDate { get; set; }
-        public bool NeedsRevision => DateTime.Now - LastRevisionDate > TimeSpan.FromDays(365 * 2);
-
-        public bool IsInEditMode { get; set; }
-
-        public VybaveniModel(string name, DateTime BoughtData, DateTime LastRevisionDate)
+        List<VybaveniModel> list = new();
+        Random rnd = new();
+        for (int i = 0; i < 20; i++)
         {
-            this.name = name;
-            this.BoughtData = BoughtData;
-            this.LastRevisionDate = LastRevisionDate;
-            IsInEditMode = false;
+            VybaveniModel model = new()
+            {
+                Name = RandomString(rnd.Next(5, 25), rnd),
+                PriceCzk = rnd.Next(5000, 10_000_000),
+                BoughtDateTime = DateTime.Now.AddDays(-rnd.Next(3 * 365, 20 * 365)),
+                LastRevision = DateTime.Now.AddDays(-rnd.Next(0, 3 * 365)),
+            };
+
+            //model.
+
+            list.Add(model);
         }
+        return list;
+
     }
+    public static string RandomString(int length, Random rnd) =>
+        new(Enumerable.Range(0, length).Select(_ => (char)rnd.Next('a', 'z')).ToArray());
+
+
+    public VybaveniModel Copy()
+    {
+        VybaveniModel to = new();
+        to.BoughtDateTime = BoughtDateTime;
+        to.LastRevision = LastRevision;
+        to.IsInEditMode = IsInEditMode;
+        to.Name = Name;
+        to.PriceCzk = PriceCzk;
+        return to;
+    }
+
+    public void MapTo(VybaveniModel? to)
+    {
+        if (to == null) return;
+        to.BoughtDateTime = BoughtDateTime;
+        to.LastRevision = LastRevision;
+        to.Name = Name;
+        to.PriceCzk = PriceCzk;
+    }
+
+
 }
