a2b79cee8678c1e96c5ccb65f52ba5cda3970540
verci <vercik.buzkova@centrum.cz>
2022-04-12 16:41:24 +0200

    test09

---
+++ PptNemocnice.Shared/RevizeModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PptNemocnice.Shared
+{
+    public class RevizeModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = "";
+
+        public static List<RevizeModel> GetTestList()
+        {
+            List<RevizeModel> list = new();
+            Random rnd = new();
+            for (int i = 0; i < 20; i++)
+            {
+                RevizeModel model = new()
+                {
+                    Id = Guid.NewGuid(),
+                    Name = RandomString(rnd.Next(5, 25), rnd)                    
+                };
+                //model.
+                list.Add(model);
+            }
+            return list;
+
+        }
+        public static string RandomString(int length, Random rnd) =>
+        new(Enumerable.Range(0, length).Select(_ => (char)rnd.Next('a', 'z')).ToArray());
+    }
+}
